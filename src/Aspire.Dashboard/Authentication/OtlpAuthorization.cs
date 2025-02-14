36ebd4d76ca7e2bebd5b3b595558da23deee2f74
Robinson Batista <manuel7@outlook.es>
2025-02-14 12:16:08 -0500

    add OtlpAuthorization.PolicyName metadata to endpoint

---
+++ src/Aspire.Dashboard/Authentication/OtlpAuthorization.cs
@@ -0,0 +1,7 @@
+﻿namespace Aspire.Dashboard.Authentication;
+
+public static class OtlpAuthorization
+{
+    public const string PolicyName = "OtlpPolicy";
+    public const string OtlpClaimName = "OtlpClaim";
+}
