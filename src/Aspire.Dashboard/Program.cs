a157dc1b05aae4c57b2f412852df8d4ec5933b8b
Robinson Batista <manuel7@outlook.es>
2025-02-14 12:18:31 -0500

    add OTLP/gRPC and OTLP/HTTP services to the DI container

--- src/Aspire.Dashboard/Program.cs
+++ src/Aspire.Dashboard/Program.cs
@@ -1,5 +1,3 @@
-using Aspire.Dashboard.Components;
-
 namespace Aspire.Dashboard;
 
 public class Program
@@ -8,27 +6,15 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents();
+        var startup = new Startup(configuration: builder.Configuration);
+
+        startup.ConfigureServices(services: builder.Services);
+
+
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAntiforgery();
-
-        app.MapStaticAssets();
-        app.MapRazorComponents<App>()
-            .AddInteractiveServerRenderMode();
+        startup.Configure(app: app, env: app.Environment);
 
         app.Run();
     }
---
+++ src/Aspire.Dashboard/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Aspire.Dashboard.Components;
+using Aspire.Dashboard.Otlp;
+using Aspire.Dashboard.Otlp.Grpc;
+using Aspire.Dashboard.Otlp.Http;
+
+namespace Aspire.Dashboard;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    public IServiceCollection Services { get; private set; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        Services = services;
+
+        // Add services to the container.
+        Services.AddGrpc();
+
+        Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+
+        // OTLP services.
+        services.AddTransient<OtlpLogsService>();
+        services.AddTransient<OtlpTraceService>();
+        services.AddTransient<OtlpMetricsService>();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        // +
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        // OTLP HTTP services.
+        app.MapHttpOtlpApi(/*dashboardOptions.Otlp*/);
+
+        // OTLP gRPC services.
+        app.MapGrpcService<OtlpGrpcTraceService>();
+        app.MapGrpcService<OtlpGrpcMetricsService>();
+        app.MapGrpcService<OtlpGrpcLogsService>();
+    }
+}
